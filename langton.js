f91bf7b8f5f99483ffe9b094da794a9b1ab8506d
wrigzilla <matt.wrigley@ntlworld.com>
2015-01-11 15:34:53 +0000

    Removed width debugging

--- langton.js
+++ langton.js
@@ -53,17 +53,13 @@ function antAlgorithm( canvas, ctx, squareColour, colour, panel, repeatID )
 	
 function init()
 {
-//	var output = document.getElementById( "output" ) ;
 	var canvas = document.getElementById( "board" ) ;
 	var ctx = canvas.getContext( "2d" ) ;
 	var panel = document.getElementById( "panel" ) ;
 	var squareColour = false ; 
 	var colour = "white" ;
-//	var width = document.getElementById("wrapper").offsetWidth;
 	var width = window.innerWidth ;
-//	output.innerHTML += width ;
-	document.getElementById("langton").style.marginLeft = (width - 600)/5 + "px" ;
-//	output.innerHTML += "<br>" + (width - 600) / 5 ;
+	document.getElementById("langton").style.marginLeft = ( width - 600 ) / 5 + "px" ;
 	ctx.fillStyle = colour ; 
 	ctx.fillRect( 0 , 0 , 600 , 600 ) ;
 	var repeat = setInterval( function() {antAlgorithm( canvas, ctx, squareColour, colour, panel, repeat );}, 1 ) ;
